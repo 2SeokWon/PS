edbcdb349179d017cfe802652aba154431ed3b92
2SeokWon <86522000+2SeokWon@users.noreply.github.com>
2023-05-05 13:23:21 +0000

    BOJ

---
+++ BOJ/10973.py
@@ -0,0 +1,15 @@
+import sys; input = sys.stdin.readline
+
+N = int(input())
+lst = list(map(int,input().split()))
+
+for i in range(N-1,0,-1):
+    if lst[i] < lst[i-1]:
+        for j in range(N-1,0,-1):
+            if lst[j] < lst[i-1]:
+                lst[j],lst[i-1] = lst[i-1],lst[j]
+                lst = lst[:i] + sorted(lst[i:],reverse = True)
+                print(" ".join(map(str,lst)))
+                exit()
+
+print(-1)
